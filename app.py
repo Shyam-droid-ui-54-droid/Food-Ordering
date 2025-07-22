771e673d9a8c9b815620f9cce4771703a2faa25e
BuildTools <unconfigured@null.spigotmc.org>
2025-07-22 23:56:43 +1200

    Created base html and seperate pages

--- app.py
+++ app.py
@@ -25,16 +25,17 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+#Route for the home page which is the menu
 @app.route('/')
-def home():
+def menu():
     #home page- menu so name, price, food type
     sql = """
                 SELECT * FROM Food
                 """ 
     results = query_db(sql)
-    return str(results)
-#Later to be organised depending on website layout / price / type etc
+    return render_template("menu.html", foods=results)
 
+#Route for the food information page / to be made into modal
 @app.route("/food/<int:food_id>")
 def food(food_id):
     food_id = int(food_id)
@@ -45,6 +46,20 @@ def food(food_id):
     results = query_db(sql, (food_id,), True)
     return str(results)    
 
+@app.route("/offers")
+def offers():
+    #offers page- shows all offers available
+    return render_template("offers.html")
+
+@app.route ("/rewards")
+def rewards():
+    #rewards page- displays customer loyalty rewards
+    return render_template("rewards.html")
+
+@app.route ("/locations")
+def locations():
+    #locations page - displays location information of restraunts
+    return render_template("locations.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
