f79ea35e5db7c54ce3dff9507ea5777413fa61b2
BuildTools <unconfigured@null.spigotmc.org>
2025-07-30 19:59:23 +1200

    Created grid styling css for menu layout

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row # In order to access columns by name
     return db
 
 @app.teardown_appcontext
